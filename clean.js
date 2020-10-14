2a5c2069f67263d009e30954eda777d07a06e306
Tim Sampson <timsampson@gmail.com>
2020-10-14 04:19:50 +0000

    update bld

--- clean.js
+++ clean.js
@@ -1,14 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
-
-
 const distDir = "dist";
-
-// Creates /tmp/a/apple, regardless of whether `/tmp` and /tmp/a exist.
-fs.mkdir('dist', { recursive: true }, (err) => {
-    if (err) throw err;
-});
 
 fs.readdir(distDir, (err, files) => {
     if (err) throw err;
--- util.js
+++ util.js
@@ -6,6 +6,10 @@ const finalDir = "appsscript";
 const distDir = "dist";
 const htmlFile = "index.html";
 
+// Creates /tmp/a/apple, regardless of whether `/tmp` and /tmp/a exist.
+fs.mkdir('dist', { recursive: true }, (err) => {
+    if (err) throw err;
+});
 
 fs.copyFileSync(path.resolve(__dirname, distDir, htmlFile), path.resolve(__dirname, finalDir, htmlFile));
 

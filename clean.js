94b5fe06e9a43da8967505a7c3b8084544f7c1bb
Tim Sampson <timsampson@gmail.com>
2020-10-17 10:30:04 +0800

    built steps

--- clean.js
+++ clean.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const distDir = "dist";
 
+// If app is cloned from git, it won't contain a dist folder and will have an error on build.
+fs.mkdir('dist', { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+// delete files in the build folder
 fs.readdir(distDir, (err, files) => {
     if (err) throw err;
 
--- util.js
+++ util.js
@@ -6,11 +6,6 @@ const finalDir = "appsscript";
 const distDir = "dist";
 const htmlFile = "index.html";
 
-// If app is cloned from git, it won't contain a dist folder and will have an error on build.
-fs.mkdir('dist', { recursive: true }, (err) => {
-    if (err) throw err;
-});
-
 fs.copyFileSync(path.resolve(__dirname, distDir, htmlFile), path.resolve(__dirname, finalDir, htmlFile));
 
 const dir = fs.readdirSync(path.resolve(__dirname, servDir));
